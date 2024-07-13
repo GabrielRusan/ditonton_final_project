804281af5eb32e3ae7d265c23bb45ece1e8b4dad
gabisaCSS <gtrmastah@gmail.com>
2024-07-13 17:46:35 +0700

    add codemagic configuration and test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+# Run tests for core module
+# cd core
+# flutter test
+
+# Run tests for fitur_movie module
+cd features/feature_movie
+flutter test
+
+# Run tests for fitur_tv module
+cd ../feature_movie
+flutter test
+
+# Return to root directory
+cd ../..
